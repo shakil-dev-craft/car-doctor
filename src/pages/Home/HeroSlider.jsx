2d5ce97c7be1c9888896c51c9a917bb37918daa5
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-03-22 12:21:20 +0600

    working hero slider

--- src/pages/Home/HeroSlider.jsx
+++ src/pages/Home/HeroSlider.jsx
@@ -23,10 +23,10 @@ const HeroSlider = () => {
         <Slider className='my-10' {...settings}>
             <div className='relative'>
                 <img className='max-h-[600px] w-full object-fill object-top rounded-lg' src={img1} alt="" />
-                <div className=' px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full rounded-lg'>
-                  <div className=' md:space-y-5 lg:space-y-6 w-full sm:w-3/5 lg:w-2/5'>
+                <div className=' pb-5 md:pb-0 px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-end md:justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full rounded-lg'>
+                  <div className=' space-y-2 sm:space-y-3 md:space-y-4 lg:space-y-6 w-full sm:w-3/5 lg:w-2/5'>
                     <h1 className=' text-lg sm:text-3xl md:text-5xl xl:text-6xl font-semibold lg:font-bold'>Affordable Price For Car Servicing</h1>
-                    <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                    <p className='text-sm'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                     <div className='flex gap-4'>
                     <button className=" px-6 text-white hover:bg-transparent hover:border-white bg-[#FF3811] border-[#FF3811] rounded-md btn transition">Discover More</button>
                     <button className=" px-6 text-white hover:bg-[#FF3811] hover:border-[#FF3811] bg-transparent border-white rounded-md btn transition">Discover More</button>
