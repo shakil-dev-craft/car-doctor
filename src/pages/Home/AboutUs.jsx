0cbe15c14ebc1523b801b059f2261845ffb8c593
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-03-24 12:02:40 +0600

    About Us component complete of home

--- src/pages/Home/AboutUs.jsx
+++ src/pages/Home/AboutUs.jsx
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom';
 const AboutUs = () => {
     return (
         <div className="flex flex-col-reverse md:flex-row md:flex-none md:gap-12 lg:gap-24 my-24">
-            <div className="relative md:w-[40%] lg:w-[50%]">
-                <img className=' max-w-[100%] lg:max-w-[90%] md:min-h-[90%] lg:min-h-[95%] rounded-md object-fill' src={person} alt="" />
-                <img className=' absolute md:-right-6 lg:right-0 md:-bottom-4 lg:-bottom-10 border-8 border-white rounded-md shadow-lg right-0 max-w-[50%]' src={parts} alt="" />
+            <div className="relative md:w-[45%] lg:w-[50%]">
+                <img className=' max-w-[90%] md:max-w-[100%] lg:max-w-[90%] md:min-h-[90%] lg:min-h-[95%] rounded-md object-fill' src={person} alt="" />
+                <img className=' absolute md:-right-6 lg:right-0 -bottom-10 md:-bottom-4 lg:-bottom-10 border-8 border-white rounded-md shadow-lg right-0 max-w-[50%]' src={parts} alt="" />
             </div>
-            <div className='space-y-5 xl:pr-48 md:w-[60%] lg:w-[50%]'>
+            <div className='space-y-5 xl:pr-[200px] md:w-[55%] lg:w-[50%] mb-10 md:mb-0'>
                 <p className='text-5 font-bold text-[#FF3811]'>About Us</p>
-                <h2 className='text-[45px] font-bold leading-none'>We are qualified & of experience in this field</h2>
+                <h2 className=' text-3xl md:text-4xl lg:text-[45px] font-bold leading-none'>We are qualified & of experience in this field</h2>
                 <div className='space-y-5'>
                     <p className=' text-sm capitalize  text-[#737373]'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don not look even slightly believable.</p>
                     <p className=' text-sm capitalize  text-[#737373]'>the majority have suffered alteration in some form, by injected humour, or randomised words which don not look even slightly believable.</p>
