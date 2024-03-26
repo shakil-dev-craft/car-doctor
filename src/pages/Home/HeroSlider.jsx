edd7915cd58fff954aa7f83b18b000912f946063
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-03-26 16:24:32 +0600

    if i want set gap in slider then use variableWidth: false

--- src/pages/Home/HeroSlider.jsx
+++ src/pages/Home/HeroSlider.jsx
@@ -22,11 +22,12 @@ const HeroSlider = () => {
       slidesToShow: 1,
       autoplay: true,
       slidesToScroll: 1,
+      variableWidth: false
     };
 
     return (
         <Slider className='my-10' {...settings}>
-            <div className='relative rounded-lg overflow-hidden'>
+            <div className='relative rounded-lg overflow-hidden px-2'>
                 <img className='rounded-lg max-h-[600px] w-full object-fill object-top' src={img1} alt="" />
                 <div className='rounded-lg pb-5 sm:pb-16 md:pb-20 xl:pb-0 px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-end xl:justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full'>
                   <div className=' rounded-lg space-y-3 md:space-y-4 lg:space-y-6 xl:space-y-7 w-full sm:w-[58%] md:w-[60%] lg:w-[43%] xl:w-[40%]'>
@@ -43,7 +44,7 @@ const HeroSlider = () => {
                   </div>
                 </div>
             </div>
-            <div className='relative rounded-lg overflow-hidden '>
+            <div className='relative rounded-lg overflow-hidden px-2 '>
                 <img className='rounded-lg max-h-[600px] w-full object-fill object-top ' src={img2} alt="" />
                 <div className='rounded-lg pb-5 sm:pb-16 md:pb-20 xl:pb-0 px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-end xl:justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full '>
                   <div className='  rounded-lg space-y-3 md:space-y-4 lg:space-y-6 xl:space-y-7 w-full sm:w-[58%] md:w-[60%] lg:w-[43%] xl:w-[40%]'>
@@ -60,7 +61,7 @@ const HeroSlider = () => {
                   </div>
                 </div>
             </div>
-            <div className='relative rounded-lg overflow-hidden '>
+            <div className='relative rounded-lg overflow-hidden px-2 '>
                 <img className='rounded-lg max-h-[600px] w-full object-fill object-top ' src={img3} alt="" />
                 <div className='rounded-lg pb-5 sm:pb-16 md:pb-20 xl:pb-0 px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-end xl:justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full '>
                   <div className=' rounded-lg space-y-3 md:space-y-4 lg:space-y-6 xl:space-y-7 w-full sm:w-[58%] md:w-[60%] lg:w-[43%] xl:w-[40%]'>
@@ -77,7 +78,7 @@ const HeroSlider = () => {
                   </div>
                 </div>
             </div>
-            <div className='relative rounded-lg overflow-hidden '>
+            <div className='relative rounded-lg overflow-hidden px-2 '>
                 <img className='rounded-lg max-h-[600px] w-full object-fill object-top ' src={img4} alt="" />
                 <div className='rounded-lg pb-5 sm:pb-16 md:pb-20 xl:pb-0 px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-end xl:justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full '>
                   <div className=' rounded-lg space-y-3 md:space-y-4 lg:space-y-6 xl:space-y-7 w-full sm:w-[58%] md:w-[60%] lg:w-[43%] xl:w-[40%]'>
@@ -94,7 +95,7 @@ const HeroSlider = () => {
                   </div>
                 </div>
             </div>
-            <div className='relative rounded-lg overflow-hidden '>
+            <div className='relative rounded-lg overflow-hidden px-2 '>
                 <img className='rounded-lg max-h-[600px] w-full object-fill object-top ' src={img5} alt="" />
                 <div className='rounded-lg pb-5 sm:pb-16 md:pb-20 xl:pb-0 px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-end xl:justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full '>
                   <div className=' rounded-lg space-y-3 md:space-y-4 lg:space-y-6 xl:space-y-7 w-full sm:w-[58%] md:w-[60%] lg:w-[43%] xl:w-[40%]'>
@@ -111,7 +112,7 @@ const HeroSlider = () => {
                   </div>
                 </div>
             </div>
-            <div className='relative rounded-lg overflow-hidden '>
+            <div className='relative rounded-lg overflow-hidden px-2 '>
                 <img className='rounded-lg max-h-[600px] w-full object-fill object-top ' src={img6} alt="" />
                 <div className='rounded-lg pb-5 sm:pb-16 md:pb-20 xl:pb-0 px-5 md:px-14 lg:px-16 xl:px-24 flex flex-col justify-end xl:justify-center text-white bg-gradient-to-r from-[#151515] to-transparent absolute top-0 left-0 w-full h-full '>
                   <div className=' rounded-lg space-y-3 md:space-y-4 lg:space-y-6 xl:space-y-7 w-full sm:w-[58%] md:w-[60%] lg:w-[43%] xl:w-[40%]'>
--- src/pages/Home/Team.jsx
+++ src/pages/Home/Team.jsx
@@ -1,10 +1,78 @@
 import TitleUtility from "../../utility/TitleUtility";
+import Slider from "react-slick";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// image
+import team1 from '../../assets/images/team/1.jpg';
 
 const Team = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: false
+    };
+
     return (
         <div className=" my-10 md:mt-20">
             <div className="text-center">
                 <TitleUtility title="Meet Our Team" subtitle="Team" description="the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable." />
+            </div>
+
+            {/* team slide */}
+            <div className="slider-container">
+                <Slider {...settings}>
+                        <div className="px-3">
+                            <div className="border p-5 border-[#E8E8E8] rounded-lg space-y-5">
+                                <div className="bg-[#F3F3F3] rounded-lg">
+                                    <img className="rounded-lg md:h-[200px] lg:h-[210px] w-40 md:w-32 py-10 md:py-5 lg:py-10 mx-auto" src={team1} alt="" />
+                                </div>
+                                <div className="flex flex-col justify-between items-center space-y-3">
+                                    <h2 className="text-[#444444] text-xl md:text-base lg:text-2xl font-bold">Car Engine Plug</h2>
+                                    <p className=" text-[#FF3811] font-semibold text-lg ">$<span>20.00</span></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="px-3">
+                            <div className="border p-5 border-[#E8E8E8] rounded-lg space-y-5">
+                                <div className="bg-[#F3F3F3] rounded-lg">
+                                    <img className="rounded-lg md:h-[200px] lg:h-[210px] w-40 md:w-32 py-10 md:py-5 lg:py-10 mx-auto" src={team1} alt="" />
+                                </div>
+                                <div className="flex flex-col justify-between items-center space-y-3">
+                                    <h2 className="text-[#444444] text-xl md:text-base lg:text-2xl font-bold">Car Engine Plug</h2>
+                                    <p className=" text-[#FF3811] font-semibold text-lg ">$<span>20.00</span></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="px-3">
+                            <div className="border p-5 border-[#E8E8E8] rounded-lg space-y-5">
+                                <div className="bg-[#F3F3F3] rounded-lg">
+                                    <img className="rounded-lg md:h-[200px] lg:h-[210px] w-40 md:w-32 py-10 md:py-5 lg:py-10 mx-auto" src={team1} alt="" />
+                                </div>
+                                <div className="flex flex-col justify-between items-center space-y-3">
+                                    <h2 className="text-[#444444] text-xl md:text-base lg:text-2xl font-bold">Car Engine Plug</h2>
+                                    <p className=" text-[#FF3811] font-semibold text-lg ">$<span>20.00</span></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="px-3">
+                            <div className="border p-5 border-[#E8E8E8] rounded-lg space-y-5">
+                                <div className="bg-[#F3F3F3] rounded-lg">
+                                    <img className="rounded-lg md:h-[200px] lg:h-[210px] w-40 md:w-32 py-10 md:py-5 lg:py-10 mx-auto" src={team1} alt="" />
+                                </div>
+                                <div className="flex flex-col justify-between items-center space-y-3">
+                                    <h2 className="text-[#444444] text-xl md:text-base lg:text-2xl font-bold">Car Engine Plug</h2>
+                                    <p className=" text-[#FF3811] font-semibold text-lg ">$<span>20.00</span></p>
+                                </div>
+                            </div>
+                        </div>
+                </Slider>
             </div>
         </div>
     );
