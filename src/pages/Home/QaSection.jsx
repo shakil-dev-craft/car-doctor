1e007e074e194a7d2ef3a73483c313477f6dfd6c
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-03-27 17:55:01 +0600

    now home page fully responsive

--- src/pages/Home/QaSection.jsx
+++ src/pages/Home/QaSection.jsx
@@ -6,7 +6,7 @@ import icon3 from '../../assets/icons/logo3.png';
 const QaSection = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 justify-center items-center gap-5 bg-[#151515] md:py-20 rounded-lg my-20 px-5 py-10 md:px-0 md:p-0">
-            <div className='flex justify-start md:justify-center  items-center gap-5 mb-10 md:mb-0'>
+            <div className='flex sm:justify-center items-center gap-5 mb-10 md:mb-0'>
                 <div className='flex justify-center items-center'>
                     <img className='w-10 h-10' src={icon1} alt="" />
                 </div>
@@ -15,7 +15,7 @@ const QaSection = () => {
                     <p className='text-white text-xl md:text-base lg:text-2xl font-bold'>7:00 am - 9:00 pm</p>
                 </div>
             </div>
-            <div className='flex justify-start md:justify-center  items-center gap-5 mb-10 md:mb-0'>
+            <div className='flex sm:justify-center items-center gap-5 mb-10 md:mb-0'>
                 <div className='flex justify-center items-center'>
                     <img className='w-10 h-10' src={icon2} alt="" />
                 </div>
@@ -24,7 +24,7 @@ const QaSection = () => {
                     <p className='text-white text-xl md:text-base lg:text-2xl font-bold'>+2546 251 2658</p>
                 </div>
             </div>
-            <div className='flex justify-start md:justify-center  items-center gap-5 mb-10 md:mb-0'>
+            <div className='flex sm:justify-center items-center gap-5 mb-10 md:mb-0'>
                 <div className='flex justify-center items-center'>
                     <img className='w-10 h-10' src={icon3} alt="" />
                 </div>
