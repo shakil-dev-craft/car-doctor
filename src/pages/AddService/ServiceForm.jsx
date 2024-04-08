ca1b1fb1aec040d260119ab55841a39ccdd384d7
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-04-08 13:14:19 +0600

    service form customize start

--- src/pages/AddService/ServiceForm.jsx
+++ src/pages/AddService/ServiceForm.jsx
@@ -1,9 +1,9 @@
 
 const ServiceForm = () => {
     return (
-        <div className="mt-20 mb-14 bg-[#F3F3F3] rounded-lg flex justify-center items-center py-20">
+        <div className="mt-20 mb-14 md:bg-[#F3F3F3] rounded-lg flex justify-center items-center py-20">
             {/* service form */}
-            <div className="sign_up_form card shrink-0 w-full sm:max-w-1/2 md:max-w-[85%] shadow-xl bg-base-100 py-10 rounded-lg">
+            <div className="sign_up_form card shrink-0 w-full sm:max-w-1/2 md:max-w-[85%] shadow md:shadow-xl bg-base-100 py-10 rounded-lg">
                 <form className="card-body">
                     <div className="form-control">
                     <input type="text" name='service' placeholder="Service Name" className=" w-full rounded-md outline-none focus:outline-none input font-normal input-bordered" required />
