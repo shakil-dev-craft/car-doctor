ba6a77cec358a282256f9302684fbeaf50e4e68e
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-04-08 13:11:25 +0600

    service form development start

--- src/pages/AddService/AddService.jsx
+++ src/pages/AddService/AddService.jsx
@@ -1,10 +1,12 @@
 
 import MiniBanner from "../../components/MiniBanner/MiniBanner";
+import ServiceForm from "./ServiceForm";
 
 const AddService = () => {
     return (
         <div>
             <MiniBanner title="Add New Service" subName="Home/Service" />
+            <ServiceForm />
         </div>
     );
 };
---
+++ src/pages/AddService/ServiceForm.jsx
@@ -0,0 +1,24 @@
+
+const ServiceForm = () => {
+    return (
+        <div className="mt-20 mb-14 bg-[#F3F3F3] rounded-lg flex justify-center items-center py-20">
+            {/* service form */}
+            <div className="sign_up_form card shrink-0 w-full sm:max-w-1/2 md:max-w-[85%] shadow-xl bg-base-100 py-10 rounded-lg">
+                <form className="card-body">
+                    <div className="form-control">
+                    <input type="text" name='service' placeholder="Service Name" className=" w-full rounded-md outline-none focus:outline-none input font-normal input-bordered" required />
+                    </div>
+
+                    <div className="form-control">
+                    <input type="text" name='price' placeholder="Service Price" className=" w-full rounded-md outline-none focus:outline-none input font-normal input-bordered" required />
+                    </div>
+                    <div className="form-control mt-6">
+                    <button className="btn sign_up_btn rounded-md">Sign up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceForm;
