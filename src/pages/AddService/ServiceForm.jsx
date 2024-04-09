317cdb35b52165eb76ec8a4d80bc214de9316b72
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-04-09 16:19:29 +0600

    service form just ok now

--- src/pages/AddService/ServiceForm.jsx
+++ src/pages/AddService/ServiceForm.jsx
@@ -1,17 +1,34 @@
 
 const ServiceForm = () => {
     return (
-        <div className="mt-20 mb-14 md:bg-[#F3F3F3] rounded-lg flex justify-center items-center py-20">
+        <div className="mt-24 mb-20 bg-[#F3F3F3] rounded-lg flex justify-center items-center lg:py-14">
             {/* service form */}
-            <div className="sign_up_form card shrink-0 w-full sm:max-w-1/2 md:max-w-[85%] shadow md:shadow-xl bg-base-100 py-10 rounded-lg">
-                <form className="card-body">
-                    <div className="form-control">
-                    <input type="text" name='service' placeholder="Service Name" className=" w-full rounded-md outline-none focus:outline-none input font-normal input-bordered" required />
+            <div className=" bg-transparent sign_up_form card shrink-0 w-full lg:max-w-[90%] bg-base-100 rounded-lg">
+                <form className="card-body px-3">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-6 mb-4">
+                        <div className="form-control">
+                        <input type="text" name='service' placeholder="Service Name" className=" w-full rounded-md outline-none focus:outline-none input font-normal focus:border-transparent" required />
+                        </div>
+
+                        <div className="form-control">
+                        <input type="text" name='price' placeholder="Service Price" className=" w-full rounded-md outline-none focus:outline-none input font-normal focus:border-transparent" required />
+                        </div>
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-6 mb-4">
+                        <div className="form-control">
+                        <input type="text" name='text_here' placeholder="Text here" className=" w-full rounded-md input font-normal focus:outline-none focus:border-transparent" required />
+                        </div>
+
+                        <div className="form-control"> 
+                        <input type="text" name='service_type' placeholder="Service Type" className=" w-full rounded-md input font-normal focus:outline-none focus:border-transparent" required />
+                        </div>
                     </div>
 
                     <div className="form-control">
-                    <input type="text" name='price' placeholder="Service Price" className=" w-full rounded-md outline-none focus:outline-none input font-normal input-bordered" required />
+                    <textarea className="rounded-md outline-none focus:outline-none p-4" name="product_description" id="" cols="10" rows="8" placeholder="Product Description"></textarea>
                     </div>
+
                     <div className="form-control mt-6">
                     <button className="btn sign_up_btn rounded-md">Sign up</button>
                     </div>
